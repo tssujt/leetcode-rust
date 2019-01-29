2006dcfeb6048d5802694bbee344feb6f04db1f2
tssujt <contactxjw@gmail.com>
2019-01-29 15:07:04 +0800

    [122. Best Time to Buy and Sell Stock II]

--- src/best_time_to_buy_and_sell_stock_ii.rs
+++ src/best_time_to_buy_and_sell_stock_ii.rs
@@ -1,11 +1,15 @@
 impl Solution {
     pub fn max_profit(prices: Vec<i32>) -> i32 {
         let mut max_profit: i32 = 0;
-        let mut min_price = i32::max_value();
+        let len = prices.len();
+        let mut i: usize = 0;
 
-        for price in prices {
-            min_price = price.min(min_price);
-            max_profit = (price - min_price).max(max_profit);
+        while i < len - 1 {
+            if prices[i] < prices[i + 1] {
+                max_profit += prices[i + 1] - prices[i];
+            }
+
+            i += 1;
         }
 
         return max_profit;
@@ -20,7 +24,8 @@ mod test {
 
     #[test]
     fn test() {
-        assert_eq!(Solution::max_profit(vec![7, 1, 5, 3, 6, 4]), 5);
+        assert_eq!(Solution::max_profit(vec![7, 1, 5, 3, 6, 4]), 7);
+        assert_eq!(Solution::max_profit(vec![1, 2, 3, 4, 5]), 4);
         assert_eq!(Solution::max_profit(vec![7, 6, 4, 3, 1]), 0);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 pub mod best_time_to_buy_and_sell_stock;
+pub mod best_time_to_buy_and_sell_stock_ii;
 pub mod jewels_and_stones;
 pub mod longest_common_prefix;
 pub mod two_sum;
